27bb752516d8153bb631261a60ae28c26526be2a
Aditya Tiwari <tiwari60@purdue.edu>
2024-01-22 18:54:49 -0500

    Got it working for all dining halls and periods

--- scraper.py
+++ scraper.py
@@ -10,63 +10,78 @@ service = webdriver.ChromeService(executable_path=r'C:\Program Files\chromedrive
 driver = webdriver.Chrome(service=service)
 
 import pandas as pd
+import datetime
 
+periods = ['Breakfast', 'Dinner', 'Lunch', 'Late Lunch']
+dining_halls = ['Earhart', 'Ford', 'Hillenbrand', 'Wiley', 'Windsor']
 
-url = 'https://dining.purdue.edu/menus/Earhart/2024/1/21'
+today = datetime.date.today()
 
-driver.get(url)
+# dates 10 days into the future
+dates = [today + datetime.timedelta(days=i) for i in range(10)]
 
-# time for the page to load
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, 'meal'))
-)
+food_items = []
+dining_hall_items = []
+nutrition_urls = {}
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
+# TODO: triple loop is ugly, reformat
+
+for dining_hall in dining_halls:
+    for date in dates:
+        for period in periods:
+            # TODO: figure out a better way to go through valid periods
+            if period == 'Late Lunch' and dining_hall in ['Earhart', 'Wiley']:
+                continue
+            if period == 'Breakfast' and dining_hall == 'Hillenbrand':
+                period = 'Brunch'
+            if period == 'Breakfast' and dining_hall == 'Windsor':
+                continue
+
+            url = f'https://dining.purdue.edu/menus/{dining_hall}/{date.year}/{date.month}/{date.day}/{period}'
+            print(f'{date} {dining_hall} {period}')
+
+            driver.get(url)
+
+            # time for the page to load
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'meal'))
+            )
+
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+
+            stations = soup.find(class_='meal').find_all(class_='station')
+
+            for station in stations:
+                station_name = station.find(class_='station-name').text
+
+                for item in station.find(class_='station-items').find_all('div', recursive=False):
+                    item_a = item.find(class_='station-item')
+                    item_detail = item_a.find(class_='station-item-details')
+                    item_name = item_detail.find(class_='station-item-text').text
+
+                    item_tags_imgs = item_detail.find_all('img')
+                    tags = [img['title'] for img in item_tags_imgs]
+                    if item_name not in nutrition_urls:
+                        nutrition_urls[item_name] = item_a['href']
+                        dining_hall_items.append({
+                            'name': item_name, 
+                            'tags': tags,
+                            'url': item_a['href']
+                        })
+
+                    food_items.append({
+                        'station': station_name,
+                        'dining_hall': dining_hall,
+                        'date': date,
+                        'name': item_name,
+                        'period': period
+                    })
+
+food_df = pd.DataFrame(food_items)
+dining_hall_df = pd.DataFrame(dining_hall_items)
 
 driver.close()
-
-stations = soup.find(class_='meal').find_all(class_='station')
-
-
-
-class FoodItem:
-    def __init__(self, station, name, tags=[]):
-        self.station = station
-        self.name = name
-        self.tags = tags
-
-    def to_dict(self):
-        return {
-            'station': self.station,
-            'name': self.name,
-            'tags': self.tags
-        }
-    
-    def __str__(self):
-        return f'{self.station}: {self.name}'
-
-food = []
-
-for station in stations:
-    station_name = station.find(class_='station-name').text
-    print(station_name)
-
-    for item in station.find(class_='station-items').find_all('div', recursive=False):
-        item_detail = item.find(class_='station-item').find(class_='station-item-details')
-        item_name = item_detail.find(class_='station-item-text').text
-    
-        food_item = FoodItem(
-            station=station_name,
-            name=item_name,
-        )
-
-        item_tags_imgs = item_detail.find_all('img')
-     
-        tags = [img['title'] for img in item_tags_imgs]
-        food_item.tags = tags
-
-        food.append(food_item)
 
     
 
